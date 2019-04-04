de71deec3132575fa633f66774515c684e266916
Rahul Barua <rahul.ctg62@gmail.com>
2019-04-05 01:33:40 +0600

    change DBconnector class

--- Projects/src/com/cimch/springmvc/controller/DBconnector.java
+++ Projects/src/com/cimch/springmvc/controller/DBconnector.java
@@ -2,8 +2,8 @@ package com.cimch.springmvc.controller;
 
 public class DBconnector {
 
-	public String user = "cimdcconadmin";
-	public String pass="x+GLa274XV4qn";
+	public String user = "root";
+	public String pass="admin";
 	public String jdbcUrl = "jdbc:mysql://localhost:3306/cimch?useSSL=false";
 	public String driver = "com.mysql.jdbc.Driver";
 }
